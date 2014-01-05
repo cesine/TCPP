d4165a3bee3884c9aabeac7621f52376465fe23b
cesine <cesine@yahoo.com>
2014-01-05 12:44:43 -0500

    cordova plugin add org.apache.cordova.geolocation

--- platforms/ios/.staging/www/cordova_plugins.js
+++ platforms/ios/.staging/www/cordova_plugins.js
@@ -85,6 +85,34 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.file.DirectoryEntry",
         "clobbers": [
@@ -221,6 +249,7 @@ module.exports.metadata =
     "org.apache.cordova.media": "0.2.7",
     "org.apache.cordova.media-capture": "0.2.6",
     "org.apache.cordova.inappbrowser": "0.3.0",
+    "org.apache.cordova.geolocation": "0.3.5",
     "org.apache.cordova.file": "0.2.5"
 }
 // BOTTOM OF METADATA
