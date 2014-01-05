6fb12cdbe05beadd61f16b7d51339fcec9e4f284
cesine <cesine@yahoo.com>
2014-01-05 12:31:06 -0500

    cordova plugin add org.apache.cordova.media

--- platforms/ios/.staging/www/cordova_plugins.js
+++ platforms/ios/.staging/www/cordova_plugins.js
@@ -13,12 +13,27 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/MediaError.js",
+        "id": "org.apache.cordova.media.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/Media.js",
+        "id": "org.apache.cordova.media.Media",
+        "clobbers": [
+            "window.Media"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.console": "0.2.6"
+    "org.apache.cordova.console": "0.2.6",
+    "org.apache.cordova.media": "0.2.7"
 }
 // BOTTOM OF METADATA
 });
